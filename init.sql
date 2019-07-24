5f74d7561afa15a269493b46fd8e6f707b23cc0e
Daniel Jakots <danj@chown.me>
2019-07-24 10:30:26 -0400

    Reorder to ease copy paste and add how to delete

--- init.sql
+++ init.sql
@@ -4,13 +4,14 @@
 
 DROP TABLE IF EXISTS pf_ip_ban;
 CREATE TABLE pf_ip_ban (id SERIAL PRIMARY KEY, ip CIDR UNIQUE, updated_at timestamp without time zone, source character varying);
+ALTER TABLE pf_ip_ban OWNER TO api;
 INSERT INTO pf_ip_ban (ip, updated_at, source) VALUES ('209.229.0.0/16', '2019-04-07 11:11:25-07', 'emerging');
 INSERT INTO pf_ip_ban (ip, updated_at, source) VALUES ('219.229.0.2', '2019-04-14 11:11:25-07', 'emerging');
 
-ALTER TABLE pf_ip_ban OWNER TO api;
 
 DROP TABLE IF EXISTS users;
 CREATE TABLE users (api_user character varying UNIQUE, password character varying, active INTEGER);
+ALTER TABLE users OWNER TO api;
 INSERT INTO users VALUES ('test', '8d604831', 1);
 INSERT INTO USERS (api_user, password, active) VALUES ('virtie.chown.me', 'e6f2a131-9b00-4735-90c7-e0f4f4e561f1', 1);
 INSERT INTO USERS (api_user, password, active) VALUES ('db0.chown.me', '9a4be1ec-04e0-4f15-95e8-92138767216a', 1);
@@ -19,4 +20,4 @@ INSERT INTO USERS (api_user, password, active) VALUES ('ns3.chown.me', '86022c19
 INSERT INTO USERS (api_user, password, active) VALUES ('manicouagan.chown.me', '72010c86-18ab-46a5-8517-07c1027928e1', 1);
 INSERT INTO USERS (api_user, password, active) VALUES ('pancake.chown.me', 'fa2a12b2-b51f-4782-ba52-da3056aebb72', 1);
 
-ALTER TABLE users OWNER TO api;
+delete from pf_ip_ban where ip='109.190.57.29';
