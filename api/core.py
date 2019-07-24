9aea141de312035dc65b8119ef2c915b9c2cfdc1
Daniel Jakots <danj@chown.me>
2019-07-24 10:31:32 -0400

    Improve error message

--- api/core.py
+++ api/core.py
@@ -78,14 +78,14 @@ def pf_post():
     if request.form:
         post_data = request.form.to_dict()
         if "IP" not in post_data.keys() or "source" not in post_data.keys():
-            return make_response(jsonify({"error": "Bad Request"}), 400)
+            return make_response(jsonify({"error": "Bad Request: key missing"}), 400)
         message, status_code = ip_add(
             [{"IP": post_data["IP"], "source": post_data["source"]}]
         )
     elif request.json:
         (message, status_code) = ip_add(request.get_json())
     else:
-        return make_response(jsonify({"error": "Bad Request"}), 400)
+        return make_response(jsonify({"error": "Bad Request: not a form"}), 400)
     return (jsonify(message), status_code)
 
 
