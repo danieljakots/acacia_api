c5baf2759c0a102dbbcce0e0580495dc54cf0b51
Daniel Jakots <danj@chown.me>
2020-08-30 11:12:25 -0400

    Use variable instead of hardcoding

--- push.sh
+++ push.sh
@@ -2,8 +2,10 @@
 
 set -eux
 
-docker build . -t api:$(git last)-$(date +%F)
-# DOCKER_UPLOAD api:$(git last)-$(date +%F)
-docker tag api:$(git last)-$(date +%F) r.chown.me/api:$(git last)-$(date +%F)
-docker push r.chown.me/api:$(git last)-$(date +%F)
-echo "r.chown.me/api:$(git last)-$(date +%F)"
+_REGISTRY=r.chown.me
+_IMAGE=api
+
+docker build . -t ${_IMAGE}:$(git last)-$(date +%F)
+docker tag ${_IMAGE}:$(git last)-$(date +%F) ${_REGISTRY}/${_IMAGE}:$(git last)-$(date +%F)
+docker push ${_REGISTRY}/${_IMAGE}:$(git last)-$(date +%F)
+echo "${_REGISTRY}/${_IMAGE}:$(git last)-$(date +%F)"
