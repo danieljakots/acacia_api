3e9cf6c5c7b42f7460e211ab3e28e97110dfc5c5
Daniel Jakots <danj@chown.me>
2020-08-30 12:09:50 -0400

    Remove v1/healthcheck
    It was bad and it's not used.

--- api/v1_pf.py
+++ api/v1_pf.py
@@ -49,10 +49,3 @@ def pf_delete():
                 return make_response(jsonify({"error": "Bad Request"}), 400)
             tools.ip_delete(IP)
     return ("", 204)
-
-
-def healthcheck():
-    results = tools.ip_count()
-    if results < 3:
-        return make_response(jsonify({"error": "Too few results, check health"}), 500)
-    return jsonify(results)
