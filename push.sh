d4b5bf60bf88ce043ff6a0c1d5a9ef38e3d1b5bb
Daniel Jakots <danj@chown.me>
2022-06-18 19:26:06 -0400

    Build docker image without cache

--- push.sh
+++ push.sh
@@ -5,7 +5,7 @@ set -eux
 _REGISTRY=r.chown.me
 _IMAGE=api
 
-docker build . -t ${_IMAGE}:$(git last)-$(date +%F)
+docker build --pull --no-cache . -t ${_IMAGE}:$(git last)-$(date +%F)
 docker tag ${_IMAGE}:$(git last)-$(date +%F) ${_REGISTRY}/${_IMAGE}:$(git last)-$(date +%F)
 docker push ${_REGISTRY}/${_IMAGE}:$(git last)-$(date +%F)
 echo "${_REGISTRY}/${_IMAGE}:$(git last)-$(date +%F)"
